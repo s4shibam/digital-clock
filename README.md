c9cf9cc28b7f1bd6e849b46c44aa0dd55e309ea8
s4shibam <shibamsaha2002@gmail.com>
2023-09-01 04:51:17 +0530

    feat: added date and day along with time

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
-### Digital Clock - Clock with Animated Background
+### Clock - A Digital Clock (Time + Date + Day)
 
 **Details**
 
-- This is a simple responsive Digital Clock Web Application.
+- This is a simple responsive Digital Clock.
+- The current date and day can be seen.
 - The look & design has been enhanced with a rainbow color changing effect along with an background animation of moving colorful bubbles.
 
 **Tech Stack**
 
-- This Digital Clock Web Application is made with HTML, CSS and Vanilla Javascript only.
+[![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=HTML5&logoColor=white)](#)
+[![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=CSS3&logoColor=white)](#)
+[![JS](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=JavaScript&logoColor=black)](#)
 
 **Live Link**
 
-- [Digital Clock Web App](https://s4shibam-digitalclock.netlify.app/)
+- [Digital Clock Web App](https://clock.shibamsaha.dev/)
+<!-- - Alternate Links [1](https://s4shibam-clock.netlify.app/), [2](https://s4shibam-clock.vercel.app/) -->
 
 **Reference Image**
 
-<img src="./media/Clock.png" alt="Clock" style="width: 75%; height: auto"/>
+<img src="./media/clock.png" alt="Clock" width="640px"/>
 
 <br/>
 
--- script.js
+++ script.js
@@ -1,25 +1,60 @@
 setInterval(() => {
-    const time = document.querySelector("#time");
-    let date = new Date();
-    let hour = date.getHours();
-    let minute = date.getMinutes();
-    let second = date.getSeconds();
-    let period = "AM";
+  const clockDisplay = document.querySelector('#clock');
+  const periodDisplay = document.querySelector('#period');
+  const dateDisplay = document.querySelector('#date');
+  const dayDisplay = document.querySelector('#day');
+  let date = new Date();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let second = date.getSeconds();
+  let period = 'AM';
 
-    if (hour > 12) {
-        period = "PM";
-        hour -= 12;
-    }
-    if (hour < 10) {
-        hour = "0" + hour;
-    }
-    if (minute < 10) {
-        minute = "0" + minute;
-    }
-    if (second < 10) {
-        second = "0" + second;
-    }
-    time.textContent = hour + ":" + minute + ":" + second + " " + period;
+  if (hour > 12) {
+    period = 'PM';
+    hour -= 12;
+  }
+  if (hour < 10) {
+    hour = '0' + hour;
+  }
+  if (minute < 10) {
+    minute = '0' + minute;
+  }
+  if (second < 10) {
+    second = '0' + second;
+  }
+  clockDisplay.textContent = `${hour}:${minute}:${second}`;
+  periodDisplay.textContent = period;
+
+  // Display today's date and day
+  const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ];
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+  const dayOfWeek = daysOfWeek[date.getDay()];
+  const month = months[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+  dateDisplay.textContent = `${month} ${day}, ${year}`;
+  dayDisplay.textContent = dayOfWeek;
 }, 1000);
 
 // Developed with love by Shibam Saha
